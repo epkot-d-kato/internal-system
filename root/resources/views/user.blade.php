b070c0934249527cf2c577ef127bb7692565263a
d.kato <daisuke.kato@epkotsoftware.co.jp>
2023-08-31 15:19:44 +0900

    【cssファイル名の変更】
    ・消去
    home.css
    
    ・追加
    side_header.css
    main_home.css
    user_main.css

--- root/resources/views/user.blade.php
+++ root/resources/views/user.blade.php
@@ -3,9 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "css/home.css">
+    
     <title>Document</title>
 </head>
 <body>
-    <p>社員一覧</p>
+    <section class ="header_menu">
+        <div class = "title">
+            <p>web日報登録</p>
+        </div>
+        <div class = "menu">
+            <ul>
+                <li>自分の名前</li>
+                <li>ログアウト</li>
+            </ul>
+
+        </div>
+
+    </section>
+
+    <section class = "side_menu">
+        <div class = "content">
+            <p><a href="/home">トップページ</a></p>
+            <p><a href="">申請管理</a></p>
+            <p><a href="">社員管理</a></p>
+            <p><a href="">休日設定</a></p>
+            <p><a href="">勤怠管理</a></p>
+            <p><a href="">交通費整理</a></p>
+            <p><a href="">備品管理</a></p>
+        </div>
+
+        <div class = "blank">
+            <p></p>
+        </div>
+
+    </section>
+
+    <section class = main>
+
+    </section>
+
 </body>
 </html>
