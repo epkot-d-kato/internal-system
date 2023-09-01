8286341d62eef1f0356991bbd091966b5d6cbdb3
d.kato <daisuke.kato@epkotsoftware.co.jp>
2023-09-01 15:32:37 +0900

    【社員一覧ページ作成下書き】 ・表はダミー ・javascript 未実装

--- root/resources/views/user.blade.php
+++ root/resources/views/user.blade.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href = "css/home.css">
+    <link rel="stylesheet" href="css/side_header.css">
+    <link rel="stylesheet" href="css/user_main.css">
 
     <title>Document</title>
 </head>
+
 <body>
-    <section class ="header_menu">
-        <div class = "title">
+    <section class="header_menu">
+        <div class="title">
             <p>web日報登録</p>
         </div>
-        <div class = "menu">
+        <div class="menu">
             <ul>
                 <li>自分の名前</li>
                 <li>ログアウト</li>
@@ -22,8 +25,8 @@
 
     </section>
 
-    <section class = "side_menu">
-        <div class = "content">
+    <section class="side_menu">
+        <div class="content">
             <p><a href="/home">トップページ</a></p>
             <p><a href="">申請管理</a></p>
             <p><a href="">社員管理</a></p>
@@ -33,15 +36,148 @@
             <p><a href="">備品管理</a></p>
         </div>
 
-        <div class = "blank">
+        <div class="blank">
             <p></p>
         </div>
 
     </section>
 
-    <section class = main>
+    <section class="main">
+
+        <div class = "register_button">
+            <button>戻る</button>
+            <button>新規登録</button>
+        </div>
+
+
+        <table border="1">
+            <tr>
+                <th>社員名</th>
+                <th>入社日</th>
+                <th>詳細</th>
+                <th>編集</th>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+            <tr>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+                <td>ダミー</td>
+            </tr>
+
+
+        </table>
+
+
+        <div class = "main_controller">
+            <div class="table_controller">
+                <form action="">
+                    <select name="user" id="">
+                        <option value="">15</option>
+                        <option value="">50</option>
+                        <option value="">100</option>
+                    </select>
+                </form>
+            </div>
+
+            <div class="pagination">
+                <ul>
+                    <li><</li>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>...</li>
+                    <li>13</li>
+                    <li>></li>
+                </ul>
+            </div>
+
+            <div class="return_button">
+                <button>戻る</button>
+            </div>
+        </div>
+
 
     </section>
 
 </body>
+
 </html>
